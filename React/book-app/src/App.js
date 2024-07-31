c4938b4c84d04d23b468147f0bed5e873af18394
Pranathi Chamarthi <150171527+psycompsci@users.noreply.github.com>
2024-07-31 20:42:25 +0000

    changes to fastapi-react connection

--- React/book-app/src/App.js
+++ React/book-app/src/App.js
@@ -1,61 +1,43 @@
 // {useState, useEffect} --> React "hooks" that allow us to call API endpoints
-import React, {useState, useEffect} from 'react' ;
-import api from './api';
+import React, {useState, useEffect} from 'react'
+import api from './api'
 
 const App = () => {
   const [books, setBooks] = useState([]);
-  const [formData, setFormData] = useState({
-    book_id: '',
-    title: '',
-    authors: '', 
-    original_publication_year: ''
-  });
+  const [error, setError] = useState(null);
 
   const fetchBooks = async () => {
-    const result = await api.get('/books/{book-id}/');
-    setBooks(result.data);
-  }
+    try {
+      const result = await api.get('/books');
+      setBooks(result.data);
+    }
+    catch (error) {
+      setError('Error getting books');
+    }
+  };
 
   useEffect(() => {
     fetchBooks();
   }, []);
 
-  const handleInputChange = (event) => {
-    // const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
-    setFormData({
-      ...formData, 
-      [event.target.name]: event.target.value
-    });
+  if (error) {
+    return <div><h1>{error}</h1></div>;
   }
-
-  const handleFormSubmit = async(event) => {
-    event.preventDefault();
-    await api.post('/books/{book-id}/', formData);
-    fetchBooks();
-    setFormData({
-      book_id: '', 
-      title: '', 
-      authors: '', 
-      original_publication_year: ''
-    });
-  };
 
   return (
     <div>
       <nav className='navbar navbar-dark bg-primary'>
         <div className='container-fluid'>
-          <a className='navbar-brand' href='#'>Book App</a>
+          <a className='navbar-brand' href='#top'>Book App</a>
         </div>
       </nav>
-      
-      <p>Welcome to the book recommendation app!</p>
-      
+
       <table className='table table-striped table-bordered table-hover'>
         <thead>
           <tr>
             <th>Book ID</th>
             <th>Title</th>
-            <th>Author(s)</th>
+            <th>Author</th>
             <th>Original Publication Year</th>
           </tr>
         </thead>
@@ -69,11 +51,9 @@ const App = () => {
             </tr>
           ))}
         </tbody>
-
-
       </table>
     </div>
-  )
+  );
 }
 
 export default App;
--- React/book-app/src/api.js
+++ React/book-app/src/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
+const PORT_NUMBER = 8000;
+
 // connecting to FastAPI @ port 8000
 const api = axios.create({
-  baseURL: 'http://localhost/8000',
-  
+  baseURL: process.env.REACT_APP_CODESPACE_NAME ? "https://" + process.env.REACT_APP_CODESPACE_NAME + "-" + PORT_NUMBER + ".app.github.dev" : "random",
 })
 
 export default api;
